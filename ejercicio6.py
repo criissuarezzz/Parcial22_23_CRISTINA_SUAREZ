b8565c4a336125474291f7e9a8b98b7b6405d3ab
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-30 09:27:44 +0200

    commit

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,5 +1,4 @@
-def sarrusiterativo():
-    print('\033[35m'+ "DETERMINANTE DE UNA MATRIZ 5X5 DE MANERA ITERATIVA " + '\033[0m')
+def determinante():
     matriz = []
     for i in range(5):
         matriz.append([])
@@ -26,3 +25,26 @@ def sarrusiterativo():
             print(matriz[i])
     else:
         print("la matriz no se modificará")
+    print('\033[36m'+"¿desea calcular el determinante de manera iterativa o recursiva? (iterativa/recursiva)"+'\033[0m')
+    respuesta = input("->")
+    if respuesta == "iterativa":
+        print('\033[35m'+ "DETERMINANTE DE UNA MATRIZ 5X5 DE MANERA ITERATIVA " + '\033[0m')
+        determinante=matriz[0][0]*matriz[1][1]*matriz[2][2]*matriz[3][3]*matriz[4][4]+matriz[0][1]*matriz[1][2]*matriz[2][3]*matriz[3][4]*matriz[4][0]+matriz[0][2]*matriz[1][3]*matriz[2][4]*matriz[3][0]*matriz[4][1]+matriz[0][3]*matriz[1][4]*matriz[2][0]*matriz[3][1]*matriz[4][2]+matriz[0][4]*matriz[1][0]*matriz[2][1]*matriz[3][2]*matriz[4][3]-matriz[0][4]*matriz[1][3]*matriz[2][2]*matriz[3][1]*matriz[4][0]-matriz[0][3]*matriz[1][2]*matriz[2][1]*matriz[3][0]*matriz[4][4]-matriz[0][2]*matriz[1][1]*matriz[2][0]*matriz[3][4]*matriz[4][3]-matriz[0][1]*matriz[1][0]*matriz[2][4]*matriz[3][3]*matriz[4][2]-matriz[0][0]*matriz[1][4]*matriz[2][3]*matriz[3][2]*matriz[4][1]
+        print("el determinante de la matriz es: ", determinante)
+    elif respuesta == "recursiva":
+        print('\033[35m'+ "DETERMINANTE DE UNA MATRIZ 5X5 DE MANERA RECURSIVA " + '\033[0m')
+        def determinante(matriz):
+            if len(matriz) == 1:
+                return matriz[0][0]
+            else:
+                determinante = 0
+                for i in range(len(matriz)):
+                    determinante += (-1)**i * matriz[0][i] * determinante([fila[:i] + fila[i+1:] for fila in (matriz[1:])])
+                return determinante
+        print("el determinante de la matriz es: ", determinante(matriz))
+    else:
+        print("opcion no valida")
+        interorecur()
+
+if __name__=="__main__":
+    determinante()
