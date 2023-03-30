b22d629bf8964e302041c8086379ba1d72fa9aa8
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-30 09:01:39 +0200

    commit

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,11 +4,23 @@ class Alumno:
         self.nota=nota
 
     def __str__(self):
-        print("Nombre: "+self.nombre+" Nota: "+str(self.nota)+". Alumno creado con éxito.")
+        print("Nombre: "+self.nombre+". Nota: "+str(self.nota)+". Alumno creado con éxito.")
 
     def calificacion(self):
         if self.nota>=5:
             print("El alumno", self.nombre, "ha aprobado con un", self.nota)
         else:
             print("El alumno", self.nombre, "ha suspendido con un", self.nota)
-            
+
+
+alumno1=Alumno("Pedro", 5)
+alumno1.__str__()
+alumno1.calificacion()
+
+alumno2=Alumno("Juan", 6.3)
+alumno2.__str__()
+alumno2.calificacion()
+
+alumno3=Alumno("Ana", 4.2)
+alumno3.__str__()
+alumno3.calificacion()
