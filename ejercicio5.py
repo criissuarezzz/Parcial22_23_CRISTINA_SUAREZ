b2c4690745dca3c7986d4a73ee421411f17dc6ad
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-30 09:10:23 +0200

    commit

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,12 +1,34 @@
 # cada carácter deberá ser encriptado a ocho caracteres;
 #se deberá generar dos tablas hash para encriptar y desencriptar, para los caracteres desde el “ ” hasta el “}” –es decir desde el 32 al 125 de la tabla ASCII.
+class Node:
+    def __init__(self, value):
+        self.value=value
+        self.next=None
 
-
-class Encriptar:
+class Encriptador:
     def __init__(self, texto):
         self.texto=texto
-        self.tabla_encriptar={}
-        self.tabla_desencriptar={}
         self.encriptado=""
         self.desencriptado=""
-        self.generar_tablas()
+        self.encriptar()
+        self.desencriptar()
+
+    def encriptar(self):
+        for i in self.texto:
+            self.encriptado+=chr(ord(i)+8)
+
+    def desencriptar(self):
+        for i in self.encriptado:
+            self.desencriptado+=chr(ord(i)-8)
+
+    def __str__(self):
+        return "Texto encriptado: "+self.encriptado+". Texto desencriptado: "+self.desencriptado+"."
+
+def Encriptar(texto):
+    encriptador=Encriptador(texto)
+    return encriptador
+
+
+texto=Encriptar("el desarrollo de un algoritmo de encriptacion para las comunicaciones rebeldes")
+print(texto)
+
