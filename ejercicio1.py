2779a1f7ff836e5ce328e84a498651dbc821eab3
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-30 08:55:41 +0200

    commit

--- ejercicio1.py
+++ ejercicio1.py
@@ -38,11 +38,15 @@ class Lista:
                 j+=1
                 k+=1
         return lista
-    
-
+    #Dada la lista anterior y un valor 145 devolver el índice de 145 en la lista si 145 está en la lista, y −1 si 145 no está en la lista
+    def indice(self, valor):
+        for i in self.lista:
+            if i==valor:
+                print("El indice del valor", valor, "es", self.lista.index(valor))
+            else:
+                return -1
 
 lista=Lista([18, 50, 210, 80, 145, 333, 70, 30])
 lista.multiplo()
 print("La lista ordenada es: ", lista.mezcla([18, 50, 210, 80, 145, 333, 70, 30]))
-
-
+lista.indice(145)
