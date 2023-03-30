50f25f4d3e299b02a3e14f7a7a391ea426eb9664
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-30 09:33:28 +0200

    commit

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,14 +1,16 @@
-def determinante():
-    matriz = []
+def sarrus5x5iterativo():
+    print('\033[35m'+ "DE MANERA ITERATIVA " + '\033[0m')
+    matriz=[]
     for i in range(5):
         matriz.append([])
         for j in range(5):
-            matriz[i].append(int(input("Introduce el valor de la posición (" + str(i) + "," + str(j) + "): ")))
-    print("La matriz es: ")
+            matriz[i].append(int(input("ingrese el numero de la posicion ["+str(i)+"]["+str(j)+"]: ")))
+    print("la matriz es: ")
     for i in range(5):
-        print(matriz[i])
-
-
+        for j in range(5):
+            print(matriz[i][j], end=" ")
+        print()
+        
     print('\033[36m'+"¿desea cambiar algún numero de la matriz? (si/no)"+'\033[0m')
     respuesta = input("->")
     if respuesta == "si":
@@ -20,31 +22,18 @@ def determinante():
         print("ingrese el nuevo numero: ")
         numero = int(input("->"))
         matriz[fila][columna] = numero
-        print("la matriz ahora es: ")
+        print("la matriz es: ")
         for i in range(5):
-            print(matriz[i])
+            for j in range(5):
+                print(matriz[i][j], end=" ")
+            print()
     else:
-        print("la matriz no se modificará")
-    print('\033[36m'+"¿desea calcular el determinante de manera iterativa o recursiva? (iterativa/recursiva)"+'\033[0m')
-    respuesta = input("->")
-    if respuesta == "iterativa":
-        print('\033[35m'+ "DETERMINANTE DE UNA MATRIZ 5X5 DE MANERA ITERATIVA " + '\033[0m')
-        determinante=matriz[0][0]*matriz[1][1]*matriz[2][2]*matriz[3][3]*matriz[4][4]+matriz[0][1]*matriz[1][2]*matriz[2][3]*matriz[3][4]*matriz[4][0]+matriz[0][2]*matriz[1][3]*matriz[2][4]*matriz[3][0]*matriz[4][1]+matriz[0][3]*matriz[1][4]*matriz[2][0]*matriz[3][1]*matriz[4][2]+matriz[0][4]*matriz[1][0]*matriz[2][1]*matriz[3][2]*matriz[4][3]-matriz[0][4]*matriz[1][3]*matriz[2][2]*matriz[3][1]*matriz[4][0]-matriz[0][3]*matriz[1][2]*matriz[2][1]*matriz[3][0]*matriz[4][4]-matriz[0][2]*matriz[1][1]*matriz[2][0]*matriz[3][4]*matriz[4][3]-matriz[0][1]*matriz[1][0]*matriz[2][4]*matriz[3][3]*matriz[4][2]-matriz[0][0]*matriz[1][4]*matriz[2][3]*matriz[3][2]*matriz[4][1]
-        print("el determinante de la matriz es: ", determinante)
-    elif respuesta == "recursiva":
-        print('\033[35m'+ "DETERMINANTE DE UNA MATRIZ 5X5 DE MANERA RECURSIVA " + '\033[0m')
-        def determinante(matriz):
-            if len(matriz) == 1:
-                return matriz[0][0]
-            else:
-                determinante = 0
-                for i in range(len(matriz)):
-                    determinante += (-1)**i * matriz[0][i] * determinante([fila[:i] + fila[i+1:] for fila in (matriz[1:])])
-                return determinante
-        print("el determinante de la matriz es: ", determinante(matriz))
-    else:
-        print("opcion no valida")
-        
+        print("la matriz se queda igual")
+    determinante = matriz[0][0]*matriz[1][1]*matriz[2][2]*matriz[3][3]*matriz[4][4] + matriz[0][1]*matriz[1][2]*matriz[2][3]*matriz[3][4]*matriz[4][0] + matriz[0][2]*matriz[1][3]*matriz[2][4]*matriz[3][0]*matriz[4][1] + matriz[0][3]*matriz[1][4]*matriz[2][0]*matriz[3][1]*matriz[4][2] + matriz[0][4]*matriz[1][0]*matriz[2][1]*matriz[3][2]*matriz[4][3] - matriz[0][4]*matriz[1][3]*matriz[2][2]*matriz[3][1]*matriz[4][0] - matriz[0][3]*matriz[1][2]*matriz[2][1]*matriz[3][0]*matriz[4][4] - matriz[0][2]*matriz[1][1]*matriz[2][0]*matriz[3][4]*matriz[4][3] - matriz[0][1]*matriz[1][0]*matriz[2][4]*matriz[3][3]*matriz[4][2] - matriz[0][0]*matriz[1][4]*matriz[2][3]*matriz[3][2]*matriz[4][1] #utilizamos la fórmula del determinante de una matriz cuadrada de 5x5
+    print("el determinante de la matriz es: ", determinante)
+    print('\033[35m'+ "===================" + '\033[0m')
 
-if __name__=="__main__":
-    determinante()
+
+
+if __name__ == "__main__":
+    sarrus5x5iterativo()
